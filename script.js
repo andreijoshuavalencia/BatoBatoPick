10b52690db09f0c04e76112399eac06b650f6221
andreijoshua <valenciaandreijoshua@gmail.com>
2022-05-04 18:14:00 +0800

    finished playRound() made some changes spent lots of time looking at the screen and thinking about how will i solve now thinking about how to solve the game().

--- script.js
+++ script.js
@@ -4,23 +4,44 @@ function computerPlay(){
     let computerChoice = computerPick[Math.floor(Math.random()*computerPick.length)];
     return computerChoice;
 }
-
 function playRound(player, computer){
-
+    let roundWinner = '';
     let choice = player.toLowerCase();
-    console.log("You chose " + choice);
+    console.log("Player chose " + choice);
     console.log("Computer chose " + computer);
-    switch(choice){
-        case "rock":
-            if (computer == "paper"){
-                console.log("You win")
-            }else if (computer == "scissors"){
-                console.log("You lose");
-            }else{
-                console.log("Tied!");
-            }
+
+    if(player === computer){
+        roundWinner = "It's a tie!";
+    }else if(player == 'rock' && computer == 'scissors'){
+        roundWinner = "Win";
+    }else if(player == 'rock' && computer == 'paper'){
+        roundWinner = "Lose";
+    }else if (player == 'paper' && computer == 'rock'){
+        roundWinner = "Win";
+    }else if (player == 'paper' && computer == 'scissors'){
+        roundWinner = "Lose";
+    }else if (player == 'scissors' && computer == 'rock'){
+        roundWinner = "Lose";
+    }else if(player == 'scissors' && computer == 'paper'){
+        roundWinner = "Win";
     }
+    return roundWinner;
+}
+
+function game(){
+    let roundResult = '';
+    let playerPoints = 0;
+    for (let i = 0; i < 5; i++ ){
+        console.log(playRound(playerSelection, computerSelection));
+        if(roundResult.includes("Win")){
+            playerPoints += 1;
+        }
+        console.log(playerPoints);
+    }
+    
+
 }
 const playerSelection = "rock";
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+game();
+// console.log(playRound(playerSelection, computerSelection));
