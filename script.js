e90caccaa855f3ca621972b99aab837a28f1cbc4
andreijoshua <valenciaandreijoshua@gmail.com>
2022-05-04 17:23:16 +0800

    created play round function able to solve the game logic used the switch function finishing one round of bbp.

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 function computerPlay(){
-    let rock,paper,scissors;
+    // let rock,paper,scissors;
     const computerPick = ["rock", "paper", "scissors"];
     let computerChoice = computerPick[Math.floor(Math.random()*computerPick.length)];
-    console.log(computerChoice);
+    return computerChoice;
 }
-computerPlay();
+
+function playRound(player, computer){
+
+    let choice = player.toLowerCase();
+    console.log("You chose " + choice);
+    console.log("Computer chose " + computer);
+    switch(choice){
+        case "rock":
+            if (computer == "paper"){
+                console.log("You win")
+            }else if (computer == "scissors"){
+                console.log("You lose");
+            }else{
+                console.log("Tied!");
+            }
+    }
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
